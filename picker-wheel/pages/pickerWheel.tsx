623831eef46c11057535cc889c442305e0836ec3
Marvin <marvinnrn@gmail.com>
2023-04-15 04:37:33 +0200

    commencement et stylisation de la page picker wheel

--- picker-wheel/pages/pickerWheel.tsx
+++ picker-wheel/pages/pickerWheel.tsx
@@ -3,8 +3,21 @@ import React from 'react'
 
 export default function PickerWheel() {
     return (
-        <div className='wheelPage'>
+        <main className='wheelPage'>
             <NavBar />
-        </div>
+            <div className='wheelPage__container'>
+                <section className='wheelPage__wheelSide'>
+                    <h1>Picker Wheel</h1>
+                </section>
+                <aside className='wheelPage__promptSide'>
+                    <input
+                        className='wheelPage__input'
+                        type='text'
+                        placeholder='Entrez votre texte'
+                    />
+                    <button type='submit'>+</button>
+                </aside>
+            </div>
+        </main>
     )
 }
