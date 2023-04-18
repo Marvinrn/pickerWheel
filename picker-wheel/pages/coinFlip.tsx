70a9ce0f32cc39cd27334601065d7edb704570aa
Marvin <marvinnrn@gmail.com>
2023-04-18 02:54:46 +0200

    les valeurs s'affichent hors du cercle pour l'instant, mais elles correspondent a leur partie du cercle

--- picker-wheel/pages/coinFlip.tsx
+++ picker-wheel/pages/coinFlip.tsx
@@ -14,24 +14,27 @@ const Circle: React.FC<CircleProps> = ({ radius, values }) => {
     const segmentColors = ['#b20a2c', '#17202a', '#cf9ca6'];
 
     return (
-        <svg width={200 * 2} height={200 * 2}>
+        <svg width={400} height={400}>
             {values.map((value, index) => {
                 const startAngle = (index * step - step / 2) * (Math.PI / 180);
                 const endAngle = ((index + 1) * step - step / 2) * (Math.PI / 180);
                 const largeArcFlag = endAngle - startAngle <= Math.PI ? 0 : 1;
                 const segmentPath = `
-            M 200,200
-            L ${200 + radius * Math.cos(startAngle)}, ${200 + radius * Math.sin(startAngle)}
-            A ${radius},${radius} 0 ${largeArcFlag},1 ${200 + radius * Math.cos(endAngle)}, ${200 + radius * Math.sin(endAngle)}
-            L 200,200
-            Z
-          `;
+          M 200,200
+          L ${200 + radius * Math.cos(startAngle)}, ${200 + radius * Math.sin(startAngle)}
+          A ${radius},${radius} 0 ${largeArcFlag},1 ${200 + radius * Math.cos(endAngle)}, ${200 + radius * Math.sin(endAngle)}
+          L 200,200
+          Z
+        `;
                 const segmentColor = segmentColors[index % segmentColors.length];
+
+                const x = 200 + (200 * 0.8) * Math.cos(startAngle + (endAngle - startAngle) / 2);
+                const y = 200 + (200 * 0.8) * Math.sin(startAngle + (endAngle - startAngle) / 2);
 
                 return (
                     <React.Fragment key={index}>
                         <path d={segmentPath} fill={segmentColor} />
-                        <text x={200} y={200} textAnchor="middle" dominantBaseline="middle" fill='white'>
+                        <text x={x} y={y} textAnchor="middle" dominantBaseline="middle">
                             {value}
                         </text>
                     </React.Fragment>
