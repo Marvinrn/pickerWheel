c4a9061e82be267807f01791616372e0625c7c39
Marvin <marvinnrn@gmail.com>
2023-05-17 12:47:58 +0200

    pile ou face affiche bien une image random a la fin de la rotation, reste plus qu'a regler le design de la page et le responsive du site

--- picker-wheel/pages/coinFlip.tsx
+++ picker-wheel/pages/coinFlip.tsx
@@ -1,26 +1,50 @@
 import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
-import { useState } from 'react'
-export default function CoinFlip() {
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
 
-    const [result, setResult] = useState<string>('')
+export default function CoinFlip() {
+    const [result, setResult] = useState<string>('Face.png')
+    const [isFlipping, setIsFlipping] = useState<boolean>(false)
 
     const flipCoin = () => {
-        const random = Math.random()
-        if (random > 0.5) {
-            setResult('Face')
-        } else {
-            setResult('Pile')
+        if (isFlipping) {
+            return
         }
+        setIsFlipping(true)
+        setTimeout(() => {
+            const random = Math.random()
+            if (random > 0.5) {
+                setResult('Pile.png')
+            } else {
+                setResult('Face.png')
+            }
+            setIsFlipping(false)
+        }, 3000)
     }
+
+    useEffect(() => {
+        if (isFlipping) {
+            const timer = setTimeout(() => {
+                setIsFlipping(false)
+            }, 3000)
+            return () => clearTimeout(timer)
+        }
+    }, [isFlipping])
 
     return (
         <div className='coinFlipPage'>
             <NavBar />
+            <h1>Pile ou Face</h1>
             <div className='coinFlipPage__container'>
-                <h1>Pile ou Face</h1>
-                {result && <h2>{result}</h2>}
-                <button onClick={flipCoin}> Lancez la pièce </button>
+                <Image
+                    src={require(`../images/${result}`)}
+                    alt='image de roue'
+                    width={250}
+                    height={250}
+                    className={isFlipping ? 'coinFlipAnimation' : 'coinFlipPage__img'}
+                />
+                <button onClick={flipCoin}>Lancez la pièce</button>
             </div>
             <Footer />
         </div>
