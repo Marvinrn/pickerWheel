a15d478d16a641037998dd95ae95661ff6009b21
Marvin <marvinnrn@gmail.com>
2023-05-17 11:44:59 +0200

    logique du pile ou face fait, reste plus qu'a stylisé le tout et mettre les images

--- picker-wheel/pages/coinFlip.tsx
+++ picker-wheel/pages/coinFlip.tsx
@@ -1,10 +1,26 @@
 import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
+import { useState } from 'react'
 export default function CoinFlip() {
+
+    const [result, setResult] = useState<string>('')
+
+    const flipCoin = () => {
+        const random = Math.random()
+        if (random > 0.5) {
+            setResult('Face')
+        } else {
+            setResult('Pile')
+        }
+    }
+
     return (
         <div className='coinFlipPage'>
             <NavBar />
             <div className='coinFlipPage__container'>
+                <h1>Pile ou Face</h1>
+                {result && <h2>{result}</h2>}
+                <button onClick={flipCoin}> Lancez la pièce </button>
             </div>
             <Footer />
         </div>
